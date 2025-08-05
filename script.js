02e32ce2a9399c21f0f9e678dcf2bf9854c1d22b
tiepnq <tiepnguyenquang@tech.admicro.vn>
2025-08-05 22:49:22 +0700

    add audio file

--- script.js
+++ script.js
@@ -819,7 +819,7 @@ let galaxyAudio = null;
 
 function preloadGalaxyAudio() {
   const audioSources = [
-   "AE THEM NHAC TUY NHA"
+   "audios/Có chút ngọt ngào.mp3"
   ];
 
   const randomIndex = Math.floor(Math.random() * audioSources.length);
